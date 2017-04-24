e483fd05a86a0c91116ef6fb9b06f8b51764fc1f
Andrey Drachev <drachyov.andrew@gmail.com>
2017-04-24 19:25:17 +0300

    Update webpack.config.js
    need to specify 'babel-loader' instead of 'babel'

--- examples/tutorial/webpack.config.js
+++ examples/tutorial/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        loaders: ['babel'],
+        loaders: ['babel-loader'],
         include: __dirname + '/client'
       }
     ]
